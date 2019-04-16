7c990dcc3e6f0d5723374e9fbb6daf705b3d0c0d
Andre Berto <knerok@gmail.com>
2019-04-16 20:49:20 +0300

    task 4

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -14,3 +14,29 @@ test('second test', () => {
     '__tests__/__fixtures__/after.yml'))
     .toBe(result);
 });
+
+test('third test', () => {
+  const result = fs.readFileSync('__tests__/__fixtures__/result3.txt', 'utf-8');
+  expect(genDiff('__tests__/__fixtures__/before.ini',
+    '__tests__/__fixtures__/after.ini'))
+    .toBe(result);
+});
+
+const arr = [
+  ['__tests__/__fixtures__/before.json',
+    '__tests__/__fixtures__/after.json',
+    '__tests__/__fixtures__/result1.txt'],
+  ['__tests__/__fixtures__/before.yml',
+    '__tests__/__fixtures__/after.yml',
+    '__tests__/__fixtures__/result2.txt'],
+  ['__tests__/__fixtures__/before.ini',
+    '__tests__/__fixtures__/after.ini',
+    '__tests__/__fixtures__/result3.txt']];
+
+test.each(arr)(
+  'fourth test',
+  (filepath1, filepath2, resultpath) => {
+    const result = fs.readFileSync(resultpath, 'utf-8');
+    expect(genDiff(filepath1, filepath2)).toBe(result);
+  },
+);
--- src/parsers.js
+++ src/parsers.js
@@ -1,8 +1,10 @@
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const extParserFuncs = {
   '.json': JSON.parse,
   '.yml': yaml.safeLoad,
+  '.ini': ini.parse,
 };
 
 const parser = (ext, data) => extParserFuncs[ext](data);
