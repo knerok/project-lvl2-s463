7d612c66f0061a4586425358b414a710a97076dc
Andre Berto <knerok@gmail.com>
2019-04-16 16:27:42 +0300

    finally

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,7 +3,7 @@ import genDiff from '../src';
 
 test('first test', () => {
   const result = fs.readFileSync('__tests__/__fixtures__/result1.txt', 'utf-8');
-  expect(genDiff('/home/andrey/Desktop/pr2/__tests__/__fixtures__/before.json',
+  expect(genDiff('__tests__/__fixtures__/before.json',
     '__tests__/__fixtures__/after.json'))
     .toBe(result);
 });
